44b3cc94937152801102178cd41a543f48882a71
arky <847566733@qq.com>
2023-02-21 21:52:22 +0800

    展示页开发

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        <img className='logo' src={logo} />
       </header>
+      <div className='sider'></div>
+      <div className='content'></div>
     </div>
   );
 }
